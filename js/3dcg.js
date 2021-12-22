05e46c51723c78d04fb114b51a06edd5b8b79ace
須古純平 <Lily2oo@sukojunpeinoMacBook-Pro.local>
2021-12-22 19:27:59 +0900

    aa

--- js/3dcg.js
+++ js/3dcg.js
@@ -1,7 +1,44 @@
 $(function(){
+    $(".front").on("click", function(){
+        $("#about").stop().fadeToggle(100);
+    });
+    $("#about").on("click", function(){
+        $(this).stop().fadeToggle(100);
+    });
+});
+
+$(function(){
+    $(".back").on("click", function(){
+        $("#memory").stop().fadeToggle(100);
+    });
+    $("#memory").on("click", function(){
+        $(this).stop().fadeToggle(100);
+    });
+});
+
+$(function(){
     $(".top").on("click", function(){
-        $(this).css({
-            color:"white"
-        })
+        $("#backtm").stop().fadeToggle(100);
+    });
+    $("#backtm").on("click", function(){
+        $(this).stop().fadeToggle(100);
+    });
+});
+
+$(function(){
+    $(".bottom").on("click", function(){
+        $("#works").stop().fadeToggle(100);
+    });
+    $("#works").on("click", function(){
+        $(this).stop().fadeToggle(100);
+    });
+});
+
+$(function(){
+    $(".left").on("click", function(){
+        $("#member").stop().fadeToggle(100);
+    });
+    $("#member").on("click", function(){
+        $(this).stop().fadeToggle(100);
     });
 });
