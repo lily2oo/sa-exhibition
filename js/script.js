81794d4caf19e81b49ecd988c1a80d51ddc41b02
須古純平 <Lily2oo@sukojunpeinoMacBook-Pro.local>
2022-05-31 13:24:21 +0900

    2022.5.31

--- js/script.js
+++ js/script.js
@@ -26,43 +26,39 @@ $(function () {
   } else {
     $("#splash").css("display", "none"); //同日2回目のアクセスでローディング画面非表示
   };
-});
-$(function () {
+
   var style = '<link rel="stylesheet" href="telop.css">';
   $('head link:last').after(style);
-});
-$(function () {
-(function (d) {
-  var config = {
-      kitId: 'fcm7plc',
-      scriptTimeout: 3000,
-      async: true
-    },
-    h = d.documentElement,
-    t = setTimeout(function () {
-      h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";
-    }, config.scriptTimeout),
-    tk = d.createElement("script"),
-    f = false,
-    s = d.getElementsByTagName("script")[0],
-    a;
-  h.className += " wf-loading";
-  tk.src = 'https://use.typekit.net/' + config.kitId + '.js';
-  tk.async = true;
-  tk.onload = tk.onreadystatechange = function () {
-    a = this.readyState;
-    if (f || a && a != "complete" && a != "loaded") return;
-    f = true;
-    clearTimeout(t);
-    try {
-      Typekit.load(config)
-    } catch (e) {}
-  };
-  s.parentNode.insertBefore(tk, s)
-})(document);
-});
 
-$(function () {
+  (function (d) {
+    var config = {
+        kitId: 'fcm7plc',
+        scriptTimeout: 3000,
+        async: true
+      },
+      h = d.documentElement,
+      t = setTimeout(function () {
+        h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";
+      }, config.scriptTimeout),
+      tk = d.createElement("script"),
+      f = false,
+      s = d.getElementsByTagName("script")[0],
+      a;
+    h.className += " wf-loading";
+    tk.src = 'https://use.typekit.net/' + config.kitId + '.js';
+    tk.async = true;
+    tk.onload = tk.onreadystatechange = function () {
+      a = this.readyState;
+      if (f || a && a != "complete" && a != "loaded") return;
+      f = true;
+      clearTimeout(t);
+      try {
+        Typekit.load(config)
+      } catch (e) {}
+    };
+    s.parentNode.insertBefore(tk, s)
+  })(document);
+
   $('header').on('inview', function (event, isInView) {
     if (isInView) {
       $('svg', $(this)).addClass('active');
